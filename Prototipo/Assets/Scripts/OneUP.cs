469367d42da5ab4a6aa7e431d7b0eb21d497bc59
Jotapex2 <144637089+Jotapex2@users.noreply.github.com>
2023-11-18 14:00:21 -0300

    Cambios pequeños

--- Prototipo/Assets/Scripts/OneUP.cs
+++ Prototipo/Assets/Scripts/OneUP.cs
@@ -6,12 +6,18 @@ public class OneUP : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        var damageable = other.GetComponent<IDamageable>();
+        // Obtén el componente CharacterStatus del jugador
+        var characterStatus = other.GetComponent<CharacterStatus>();
 
-        if (damageable != null)
+        if (characterStatus != null)
         {
-            damageable.Heal(1);
-            Destroy(gameObject); // Destruye el objeto HealZone
+            // Verifica si la salud del jugador es menor a 5
+            if (characterStatus.health < 5)
+            {
+                // Si es así, cura al jugador y destruye el objeto OneUP
+                characterStatus.Heal(1);
+                Destroy(gameObject);
+            }
         }
     }
 }
